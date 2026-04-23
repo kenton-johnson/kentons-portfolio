38ac468567c0511f616890572e687f478d45ea2a
Kryker-jj <147555202+Kryker-jj@users.noreply.github.com>
2026-04-22 21:17:13 -0600

    Bootstrap showing on the frontend

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,14 @@
 <template>
+  <BContainer class="py-5">
+    <BHeader> Kenton's Project </BHeader>
+    <BButton variant="primary">Hello BootstrapVueNext!</BButton>
+  </BContainer>
+</template>
+
+
+<!-- <template>
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
-</template>
+</template> -->
